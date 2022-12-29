a69b69b514787df97bd5b02d7f47e208766b5061
Namrata Devkota <devkotanamrata03@gmail.com>
2022-12-29 09:17:42 +0545

    oop.php commit

---
+++ oop.php
@@ -0,0 +1,41 @@
+<?php
+ class Product{
+    public $name;
+    public $desc;
+    public $price;
+    function setdetails($n,$d,$p){
+        $this->name=$n;
+        $this->desc=$d;
+        $this->price=$p;
+
+    }
+    function getdetails(){
+        return $this->name." ".$this->desc." ".$this->price." ";
+    
+    }
+         } 
+class Employee{
+    public $name;
+    public $phone;
+    public $adress;
+    public $salary;
+     function setdetails($a,$b,$c,$d){
+        $this->name=$a;
+        $this->phone=$b;
+        $this->adress=$c;
+        $this->salary=$d;
+    
+     }
+     function getdetails(){
+        return $this->name." ".$this->phone." ".$this->adress." ".$this->salary." ";
+     }
+}
+
+     $prod= new Product();
+    $prod->setdetails("phone","iphone",10000);
+    echo ($prod->getdetails()."<br>");
+    $emp= new Employee();
+    $emp->setdetails("Elsa",9845127656,"ktm",10000);
+    echo ( $emp->getdetails());
+
+?>
